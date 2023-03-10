c3be88bd27d25996c5a7fd9da123ae0c5f73f420
vasip2f <vasikaranp2f@semi.in>
2023-03-10 11:53:00 +0530

    crud

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Layout/Navbar';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddUser from './User/AddUser';
+import EditUser from './User/EditUser';
+import ViewUser from './User/ViewUser';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/adduser' element={<AddUser />} />
+          <Route exact path='/edituser/:id' element={<EditUser />} />
+          <Route exact path='/viewuser/:id' element={<ViewUser />} />
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
